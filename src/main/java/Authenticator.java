75932da46f1c035839dad26a5c0cd8915ed9f7fb
Birk <rgeebt01@yahoo.com>
2022-04-18 14:40:04 -0400

    changed back to bold and putting in new branch

--- src/main/java/Authenticator.java
+++ src/main/java/Authenticator.java
@@ -3,7 +3,7 @@ import java.awt.*;
 
 public class Authenticator {
     public static void main(String[] args) {
-        UIManager.put("TextField.font", new Font("Dialog", Font.ITALIC, 14));
+        UIManager.put("TextField.font", new Font("Dialog", Font.BOLD, 14));
         String username = JOptionPane.showInputDialog("Username:");
         String password = JOptionPane.showInputDialog("Password:");
         if (username != null &&
