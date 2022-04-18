82728fe85cc1410ced6c05c2aa3622b9cc0b8dc2
Birk <rgeebt01@yahoo.com>
2022-04-18 12:28:16 -0400

    adding program pom and gitignore

---
+++ src/main/java/Authenticator.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Authenticator {
+    public static void main(String[] args) {
+        UIManager.put("TextField.font", new Font("Dialog", Font.BOLD, 14));
+        String username = JOptionPane.showInputDialog("Username:");
+        String password = JOptionPane.showInputDialog("Password:");
+        if (username != null &&
+                password != null &&
+                (
+                        (username.equals("greebt01") &&
+                                password.equals("swordfish")) ||
+                                (username.equals("bburd") &&
+                                        password.equals("preakston"))
+                )
+        ) {
+            JOptionPane.showMessageDialog(null, "You're in.");
+
+        } else {
+            JOptionPane.showMessageDialog(null, "You're suspicious.");
+        }
+    }
+}
